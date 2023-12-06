76d3982a89f4cb66abab0552b3ab89dd5b000cda
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2023-12-06 15:18:24 +0300

    Implement the Edit button and write todo to todos file.

--- Day17/gui.py
+++ Day17/gui.py
@@ -4,11 +4,12 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do: ")
 input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
 add_button = sg.Button("Add")
-todo_List = sg.Listbox(values=[],key="todo_list", )
+list_box = sg.Listbox(values=get_todos(),key="todos", enable_events=True, size=[45,10] )
+edit_button = sg.Button("Edit")
 
 
 window = sg.Window("To-do App", layout=[[label],[input_box,add_button],
-                                        [todo_List]], font=("Helvetica",20))
+                                        [list_box, edit_button]], font=("Helvetica",20))
 while True:
     event,values = window.read()
 
@@ -17,6 +18,21 @@ while True:
             todos = get_todos()
             todos.append(values["todo"].title()+"\n")
             write_todos(todos)
+            window["todos"].update(values=todos)
+
+
+        case "Edit":
+            window["todo"].update(value=values["todos"][0])
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"]
+            todos = get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo.title()+"\n"
+            write_todos(todos)
+            window["todos"].update(values=todos)
+
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
 
         case sg.WIN_CLOSED:
             break
