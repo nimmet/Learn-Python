1c6ba528686b3f1db354bd0be1675498edace4da
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-01-03 11:11:52 +0300

    add footer

--- Generate-Pdf/main.py
+++ Generate-Pdf/main.py
@@ -8,6 +8,7 @@ df = pandas.read_csv("topics.csv")
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for index, row in df.iterrows():
     pdf.add_page()
@@ -15,14 +16,19 @@ for index, row in df.iterrows():
 
     pdf.set_font(family="Times",style="B", size=24)
     pdf.set_text_color(100,100,100)
-    pdf.cell(w=0,h=12, txt=str(row["Order"]), align="L", ln=1)
-    pdf.line(10,22,200,22)
     pdf.cell(w=0,h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,34,200,34)
-    pdf.cell(w=0,h=12, txt=str(row["Pages"]), align="L", ln=1)
-    pdf.line(10,46,200,46)
+    pdf.line(10,22,200,22)
+
+    pdf.ln(265)
+    pdf.set_font(family="Times",style="I",size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0,h=10,txt=row["Topic"], align="R")
 
     for page in range(row["Pages"]-1):
         pdf.add_page()
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
