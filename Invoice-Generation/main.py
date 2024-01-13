d0ce385825a2a242e1988aeca31bb2092f85e9f5
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-01-13 12:05:30 +0300

    Add header, total price

--- Invoice-Generation/main.py
+++ Invoice-Generation/main.py
@@ -14,15 +14,13 @@ total = 0.0;
 for filepath in filepaths:
     df = pd.read_excel(filepath,sheet_name="Sheet 1")
     total_prices= df["total_price"].sum
-    print(df)
     pdf.add_page()
     filename = Path(filepath).stem
     invoice_nr, date = filename.split("-")
 
     pdf.set_font(family="Times", style="B", size=16)
     # pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=50, h=8,
-             txt=f"Invoice nr. {invoice_nr}", ln=1)
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
     pdf.set_font(family="Times", style="B", size=16)
     # pdf.set_text_color(100, 100, 100)
     pdf.cell(w=50, h=8, txt=f"Date: {date}",ln=1)
@@ -45,7 +43,6 @@ for filepath in filepaths:
         pdf.set_text_color(80, 80, 80)
 
 
-
         # Cell Data
         pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
         pdf.cell(w=60, h=8, txt=row["product_name"], border=1)
@@ -54,6 +51,21 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
 
         pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+    # Total Price column
+    total = df['total_price'].sum()
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=60, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(f"{total:.0f}"), border=1, ln=1)
+
+    pdf.set_font(family="Times", style="B", size=12)
+    # pdf.text(txt=f"The total due amount is {total:.0f} Euros.", x=10, y=75)
+    pdf.cell(w=25, h=8, txt=f"The total due amount is {total:.0f} Euros.", ln=1)
+    # pdf.image("invoices/pythonhow.png", w=10)
+
+
 
 
 
