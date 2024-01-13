7328ac5a18c6216a655375dd83c17a73dc54336a
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-01-13 11:35:54 +0300

    Add a table

--- Invoice-Generation/main.py
+++ Invoice-Generation/main.py
@@ -15,20 +15,51 @@ for filepath in filepaths:
     df = pd.read_excel(filepath,sheet_name="Sheet 1")
     total_prices= df["total_price"].sum
     print(df)
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_nr, date = filename.split("-")
+
+    pdf.set_font(family="Times", style="B", size=16)
+    # pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=50, h=8,
+             txt=f"Invoice nr. {invoice_nr}", ln=1)
+    pdf.set_font(family="Times", style="B", size=16)
+    # pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=50, h=8, txt=f"Date: {date}",ln=1)
+
+        # Header
+    columns = list(df.columns)
+    columns = [name.replace("_"," ").title() for name in columns]
+
+
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=60, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
     for index,row in df.iterrows():
-        pdf.add_page()
-        filename = Path(filepath).stem
-        invoice_nr, date = filename.split("-")
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
 
-        pdf.set_font(family="Times", style="B", size=16)
-        # pdf.set_text_color(100, 100, 100)
-        pdf.cell(w=50, h=8,
-                 txt=f"Invoice nr. {invoice_nr}", ln=1)
-        pdf.set_font(family="Times", style="B", size=16)
-        # pdf.set_text_color(100, 100, 100)
-        pdf.cell(w=50, h=8,
-                 txt=f"Date: {date}")
+
+
+        # Cell Data
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
+        pdf.cell(w=60, h=8, txt=row["product_name"], border=1)
+
+        pdf.cell(w=40, h=8, txt=str(row['amount_purchased']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+
+        pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+
+
+
+
+
         # pdf.cell(w=0, h=12,
         #          txt=str(row["product_id"])+"\t"+
         #          str(row["product_name"])+"\t"+str(row["amount_purchased"])+"\t"+str(row["price_per_unit"])+"\t"+
@@ -40,5 +71,5 @@ for filepath in filepaths:
         # pdf.line(10, 22, 200, 22)
 
 
-        pdf.output(f"pdfs/{filename}.pdf")
+    pdf.output(f"pdfs/{filename}.pdf")
 
