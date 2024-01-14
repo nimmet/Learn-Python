d6d566ec3ccbeffcbb995dc8962cf9b489669a08
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-01-14 11:10:26 +0300

    Update, Generate pdf file from text file.

--- Student-Project-1/main.py
+++ Student-Project-1/main.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import glob
 
-from click import style
 from fpdf import FPDF
 from pathlib import Path
-import re
 
 filepaths = glob.glob("text/*.txt")
 
@@ -12,12 +10,11 @@ filepaths = glob.glob("text/*.txt")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
-pattern = "\\[\d\\]*"
-
 for filepath in filepaths:
     with open(filepath) as file:
-        # content = file.readline().split("[\\d]{1,2}")
-        content  = re.split(pattern, file.readline())
+
+        content ="".join(file.readlines())
+
 
         print(content)
 
@@ -28,25 +25,9 @@ for filepath in filepaths:
         pdf.cell(w=50, h=8,
                  txt=f"{filename.capitalize()}", ln=1)
 
-
-
-        yy=25
-        for c in content:
-            pdf.set_font(family="Times", style="", size=12)
-            # pdf.text(txt=c.strip(),y=yy,x=10)
-            # yy += 10
-            pdf.multi_cell(w=0, h=6, txt=c)
-
-        # pdf.cell(w=0, h=12,
-        #          txt=str(row["product_id"])+"\t"+
-        #          str(row["product_name"])+"\t"+str(row["amount_purchased"])+"\t"+str(row["price_per_unit"])+"\t"+
-        #          str(row["total_price"]),
-        #          align="L", ln=1, border=1)
-        # # pdf.cell(w=0, h=12, txt=str(row["amount_purchased"]), align="L", ln=1, border=1)
-        # # pdf.cell(w=0, h=12, txt=str(row["price_per_unit"]), align="L", ln=1, border=1)
-        # # pdf.cell(w=0, h=12, txt=str(row["total_price"]), align="L", ln=1, border=1)
-        # pdf.line(10, 22, 200, 22)
-
+        # Add content to pdf file
+        pdf.set_font(family="Times", style="", size=12)
+        pdf.multi_cell(w=0, h=6, txt=str(content))
 
 pdf.output(f"pdfs/animal.pdf")
 
