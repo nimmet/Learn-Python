c44f9b7fa62deec2191fff508281e7463513fecd
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-01-15 11:18:36 +0300

    Get news body and title

--- App5-news-api-email/main.py
+++ App5-news-api-email/main.py
@@ -2,7 +2,7 @@ import requests
 
 
 api_key = "df8f6e675f67487fbe78f62e673e47ed"
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2023-12-14&sortBy=publishedAt&apiKey={api_key}"
+url = f"https://newsapi.org/v2/everything?q=tesla&from=2023-12-15&sortBy=publishedAt&apiKey=df8f6e675f67487fbe78f62e673e47ed"
 
 # Make request
 request = requests.get(url)
@@ -10,7 +10,12 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
+
 print(content["articles"])
 
+body = ""
+
 for article in content["articles"]:
-    print(article["title"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + "none" if article["description"] is None else article["description"] + 2*"\n"
+        print(body)
